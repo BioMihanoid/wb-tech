59ca3895f66392b062960aef36f4e1cdc87e7e79
Mikhail <pechnikovmichelle@gmail.com>
2025-07-14 02:39:50 +0300

    l1.3

---
+++ l1/3/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
+	"time"
+)
+
+/*
+	Реализовать постоянную запись данных в канал (в главной горутине).
+
+	Реализовать набор из N воркеров, которые читают данные из этого канала и выводят их в stdout.
+
+	Программа должна принимать параметром количество воркеров и при старте создавать указанное число горутин-воркеров.
+*/
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: go run main.go <workerCount>")
+		return
+	}
+
+	workerCount, err := strconv.Atoi(os.Args[1])
+	if err != nil || workerCount <= 0 {
+		fmt.Println("invalid worker count. Please provide a positive integer.")
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	mainChannel := make(chan int)
+	doneCh := make(chan struct{})
+
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	for i := 0; i < workerCount; i++ {
+		go worker(ctx, i, mainChannel, doneCh)
+	}
+
+	go func() {
+		counter := 0
+		for i := 0; ; i++ {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Main goroutine exiting")
+				close(mainChannel)
+				return
+
+			case mainChannel <- counter:
+				counter++
+				time.Sleep(100 * time.Millisecond)
+			}
+		}
+	}()
+
+	<-signalCh
+	fmt.Println("Received shutdown signal")
+	cancel()
+
+	for i := 0; i < workerCount; i++ {
+		<-doneCh
+	}
+
+	fmt.Println("Graceful shutdown complete")
+}
+
+func worker(ctx context.Context, id int, ch <-chan int, doneCh chan<- struct{}) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("Worker %d exiting\n", id)
+			doneCh <- struct{}{}
+			return
+
+		case value, ok := <-ch:
+			if !ok {
+				fmt.Printf("Worker %d channel closed, exiting\n", id)
+				doneCh <- struct{}{}
+				return
+			}
+			fmt.Printf("Worker %d received: %d\n", id, value)
+		}
+	}
+}
